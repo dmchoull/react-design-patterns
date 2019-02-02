dcccdc7978f7c6f4d7e5c67c31befb4efc90b9c0
David <dmchoull@gmail.com>
2019-02-02 13:16:52 -0500

    improve nav layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import CompoundComponet from "./pages/compound-components";
+import CompoundComponents from "./pages/compound-components";
 import Home from "./pages/home/Home";
+
+const listStyle = {
+  listStyle: "none"
+};
+
+const listItemStyle = {
+  display: "inline",
+  paddingRight: 20
+};
 
 const AppRouter = () => (
   <Router>
     <div>
       <nav>
-        <ul>
-          <li>
+        <ul style={listStyle}>
+          <li style={listItemStyle}>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/compound/">Compound Component</Link>
+          <li style={listItemStyle}>
+            <Link to="/compound/">Compound Components</Link>
           </li>
         </ul>
       </nav>
 
-      <Route path="/" exact component={Home} />
-      <Route path="/compound/" component={CompoundComponet} />
+      <div className="page-wrapper">
+        <Route path="/" exact component={Home} />
+        <Route path="/compound/" component={CompoundComponents} />
+      </div>
     </div>
   </Router>
 );
